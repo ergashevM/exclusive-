b4e512fce61336a3c52547e1a4dc24dee03b4b9c
ergashevM <mashrapovbohodir@gmail.com>
2025-04-16 17:49:32 +0500

    loyiha yangilandi

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-// next.config.js
-const path = require('path');
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-module.exports = {
-  webpack(config) {
-    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
-    config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-};
+export default nextConfig;
