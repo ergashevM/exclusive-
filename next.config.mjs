ce009dfb76f7113275e56de6328cdce755f976c9
ergashevM <156354829+ergashevM@users.noreply.github.com>
2025-04-16 17:39:50 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.js
+const path = require('path');
 
-export default nextConfig;
+module.exports = {
+  webpack(config) {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+};
